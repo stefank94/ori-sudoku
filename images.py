f75a5bccbe38d837a0c769a6a66d32ca82855a28
Stefan Kalnak <stefank@sbb.rs>
2016-06-24 22:40:47 +0200

    Ucitavanje slika i prva verzija neuronske mreze.

--- images.py
+++ images.py
@@ -15,56 +15,73 @@ def load_test_y_set():
     return load_y_set(51, 61)
 
 def load_x_set(od, do):
-    list = []
-    for i in range(od, do):
+    size = do - od
+    list = np.zeros((size * 3, 90000))
+
+    i = 0
+    for f in range(od, do):
         s = "0"
-        if od < 10:
+        if f < 10:
             s = "00"
-        img = cv2.imread("slike/easy/sudoku_easy_" + s + str(i) + ".jpg", 0)
-        list.append(img)
-        img = cv2.imread("slike/medium/sudoku_medium_" + s + str(i) + ".jpg", 0)
-        list.append(img)
-        img = cv2.imread("slike/hard/sudoku_hard_" + s + str(i) + ".jpg", 0)
-        list.append(img)
+        img = cv2.imread("slike/easy/sudoku_easy_" + s + str(f) + ".jpg", 0)
+        #img = keras.preprocessing.image.load_image("slike/easy/sudoku_easy_" + s + str(i) + ".jpg", True)
+        data = np.asarray(img, dtype="uint8")
+        list[i, :] = data.ravel()
+        i += 1
+
+        img = cv2.imread("slike/medium/sudoku_medium_" + s + str(f) + ".jpg", 0)
+        #img = keras.preprocessing.image.load_image("slike/medium/sudoku_medium_" + s + str(i) + ".jpg", True)
+        data = np.asarray(img, dtype="uint8")
+        list[i, :] = data.ravel()
+        i += 1
+
+        img = cv2.imread("slike/hard/sudoku_hard_" + s + str(f) + ".jpg", 0)
+        #img = keras.preprocessing.image.load_image("slike/hard/sudoku_hard_" + s + str(i) + ".jpg", True)
+        data = np.asarray(img, dtype="uint8")
+        list[i, :] = data.ravel()
+        i += 1
+
     return list
 
 def load_y_set(od, do):
-    list = []
-    for i in range(od, do):
+    size = do - od
+    list = np.zeros((size * 3, 81))
+
+    i = 0
+    for f in range(od, do):
+
         s = "0"
-        if od < 10:
+        if f < 10:
             s = "00"
-        file = open("slike/easy/sudoku_easy_" + s + str(i) + ".dat", "read")
-        one_file = []
-        for line in file:
-            one_row = []
-            chars = line.strip().split(' ')
-            for char in chars:
-                x = int(char)
-                one_row.append(x)
-            one_file.append(one_row)
-        list.append(one_file)
 
-        file = open("slike/medium/sudokumedium_" + s + str(i) + ".dat", "read")
-        one_file = []
+        file = open("slike/easy/sudoku_easy_" + s + str(f) + ".dat", "r")
+        j = 0
         for line in file:
-            one_row = []
             chars = line.strip().split(' ')
             for char in chars:
                 x = int(char)
-                one_row.append(x)
-            one_file.append(one_row)
-        list.append(one_file)
+                list[i, j] = x
+                j += 1
+        i += 1
 
-        file = open("slike/hard/sudoku_hard_" + s + str(i) + ".dat", "read")
-        one_file = []
+        file = open("slike/medium/sudoku_medium_" + s + str(f) + ".dat", "r")
+        j = 0
         for line in file:
-            one_row = []
             chars = line.strip().split(' ')
             for char in chars:
                 x = int(char)
-                one_row.append(x)
-            one_file.append(one_row)
-        list.append(one_file)
+                list[i, j] = x
+                j += 1
+        i += 1
+
+        file = open("slike/hard/sudoku_hard_" + s + str(f) + ".dat", "r")
+        j = 0
+        for line in file:
+            chars = line.strip().split(' ')
+            for char in chars:
+                x = int(char)
+                list[i, j] = x
+                j += 1
+        i += 1
 
     return list
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
-import numpy as np
+
+from images import *
 np.random.seed(1337)  # for reproducibility
 
 from keras.models import Sequential
@@ -11,3 +12,39 @@ from keras.utils import np_utils
 
 
 nb_epoch = 20
+X_train = load_train_x_set()
+Y_train = load_train_y_set()
+X_test = load_test_x_set()
+Y_test = load_test_y_set()
+
+print (X_train.shape)
+X_train = X_train.reshape(150, 90000)
+X_test = X_test.reshape(30, 90000)
+
+X_train = X_train.astype("float32")
+X_test = X_test.astype("float32")
+
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+
+model = Sequential()
+model.add(Dense(128, input_shape=(90000,)))
+model.add(Activation('sigmoid'))
+model.add(Dense(81))
+model.add(Activation('sigmoid'))
+
+# definisanje SGD, lr je learning rate
+sgd = SGD(lr=0.01)
+
+# kompajliranje modela (Theano) - optimizacija svih matematickih izraza
+model.compile(loss='mse', optimizer=sgd)
+
+# obucavanje neuronske mreze
+model.fit(X_train, Y_train, batch_size=10, nb_epoch=nb_epoch, show_accuracy=True, validation_data=(X_test, Y_test))
+
+# nakon obucavanje testiranje
+score = model.evaluate(X_test, Y_test, show_accuracy=True)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
