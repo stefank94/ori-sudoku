0b7e8c3d3de934157312d3d12ff9045be5897377
sebastijan94 <sebastijan94@yahoo.com>
2016-06-26 22:08:44 +0200

    prepoznavanje cele table

--- images.py
+++ images.py
@@ -1,16 +1,35 @@
 import numpy as np
 import cv2
-
+import matplotlib.pyplot as plt
 
 def get_one_image():
-    list = np.zeros((1, 90000))
     img = cv2.imread("slike/easy/sudoku_easy_010.jpg", 0)
+    img = inverte(img)
     data = np.asarray(img, dtype="uint8")
-    list[0, :] = data.ravel()
-    list = list.reshape(1, 90000)
-    list = list.astype("float32")
-    list /= 255
-    return list
+    numbers = [None]*81
+    counter = 0        
+    for i in xrange(0,9):       #secemo tablu na 91 sliku
+        for j in xrange(0,9):
+            num = data[i*33:i*33+33, j*33:j*33+33]
+            num = num[4:32, 4:32]
+            numbers[counter] = num
+            counter += 1
+   # for i in range(81):
+    #    plt.subplot(9,9,i+1)
+     #   plt.imshow(numbers[i], cmap='gray', interpolation='none') #stampa celu tablu
+    
+    for i in range(81):
+        num = numbers[i]
+        num = num.reshape(1, 784)
+        num = num.astype("float32")
+        num /= 255
+        numbers[i] = num
+    
+    return numbers
+    
+def inverte(imagem):
+    imagem = (255-imagem)
+    return imagem
 
 def load_train_x_set():
     return load_x_set(1,51)
--- main.py
+++ main.py
@@ -1,60 +1,125 @@
-from __future__ import absolute_import
-from __future__ import print_function
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 26 12:42:40 2016
 
+@author: Sebastijan
+"""
 from images import *
-np.random.seed(1337)  # for reproducibility
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (7,7) # Make the figures a bit bigger
 
+from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation
-from keras.optimizers import SGD
+from keras.layers.core import Dense, Dropout, Activation
 from keras.utils import np_utils
-from keras.callbacks import EarlyStopping
 
+nb_classes = 10
 
+save_weights = False #da li da sacuvamo tezine
+test_only = True  # da li samo vec obucili mrezu
 
-nb_epoch = 200
-X_train = load_train_x_set()
-Y_train = load_train_y_set()
-X_test = load_test_x_set()
-Y_test = load_test_y_set()
+# the data, shuffled and split between tran and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+print("X_train original shape", X_train.shape)
+print("y_train original shape", y_train.shape)
+#stampa primere slika
+'''for i in range(9):
+    plt.subplot(3,3,i+1)
+    plt.imshow(X_train[i], cmap='gray', interpolation='none')
+    plt.title("Class {}".format(y_train[i]))'''
 
-print (X_train.shape)
-X_train = X_train.reshape(150, 90000)
-X_test = X_test.reshape(30, 90000)
-
-X_train = X_train.astype("float32")
-X_test = X_test.astype("float32")
-
+'''Our neural-network is going to take a single vector for each training example, 
+so we need to reshape the input so that each 28x28 image becomes a single 784 dimensional vector.
+ We'll also scale the inputs to be in the range [0-1] rather than [0-255]'''
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
 X_train /= 255
 X_test /= 255
+print("Training matrix shape", X_train.shape)
+print("Testing matrix shape", X_test.shape)
 
-print(X_train.shape[0], 'train samples')
-print(X_test.shape[0], 'test samples')
+#Modify the target matrices to be in the one-hot format
+''' 0 -> [1, 0, 0, 0, 0, 0, 0, 0, 0]
+    1 -> [0, 1, 0, 0, 0, 0, 0, 0, 0]
+    2 -> [0, 0, 1, 0, 0, 0, 0, 0, 0]
+    etc.'''
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
 
-
+#3 layer fully connected network
 model = Sequential()
-model.add(Dense(128, input_shape=(90000,)))
-model.add(Activation('sigmoid'))
-model.add(Dense(81))
-model.add(Activation('sigmoid'))
+model.add(Dense(512, input_shape=(784,)))
+model.add(Activation('relu')) # An "activation" is just a non-linear function applied to the output
+                              # of the layer above. Here, with a "rectified linear unit",
+                              # we clamp all values below 0 to 0.
 
-# definisanje SGD, lr je learning rate
-sgd = SGD(lr=0.01)
+model.add(Dropout(0.2))   # Dropout helps protect the model from memorizing or "overfitting" the training data
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(10))
+model.add(Activation('softmax')) # This special "softmax" activation among other things,
+                                 # ensures the output is a valid probaility distribution, that is
+                                 # that its values are all non-negative and sum to 1.
 
-# kompajliranje modela (Theano) - optimizacija svih matematickih izraza
-model.compile(loss='mse', optimizer=sgd)
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=2)
 
-# obucavanje neuronske mreze
-histroy = model.fit(X_train, Y_train, batch_size=10, nb_epoch=nb_epoch, verbose=1, validation_split=0.2, validation_data=(X_test, Y_test), callbacks=[early_stopping])
+# ucitavanje snimljenih tezina iz prethodnog obucavanja
+if test_only:
+    model.load_weights('weights/tezine.hdf5')
+    model.compile(loss='categorical_crossentropy', optimizer='adam',  metrics=["accuracy"])
+else:
+    model.compile(loss='categorical_crossentropy', optimizer='adam',  metrics=["accuracy"])
+    model.fit(X_train, Y_train, show_accuracy=True,
+          batch_size=128, nb_epoch=10, verbose=0,
+          validation_data=(X_test, Y_test))
 
-# nakon obucavanje testiranje
-score = model.evaluate(X_test, Y_test)
-print('Test score:', score)
-#print('Test accuracy:', score[1])
-predictions = model.predict(get_one_image(), verbose = 1)
-predictions *= 9
-predictions = predictions.round(decimals=0)
-print(predictions)
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+if save_weights:
+    model.save_weights('weights/tezine.hdf5', overwrite=True)
+
+matrix = np.zeros((81))
+numbers = get_one_image()
+
+for i in xrange(0,81):
+    if sum(numbers[i][0]) < 5:
+        matrix[i] = 0
+    else:
+        predicted = model.predict_classes(numbers[i], verbose = 0)
+        matrix[i] = predicted
+    #predict = model.predict_proba(get_one_image())
+    #print(predict)
+    #print(max(predict[0]))
+matrix = matrix.reshape((9, 9))
+print(matrix)
+
+'''
+#provera sta je dobro predvideo a sta nije(stampa primere)
+# The predict_classes function outputs the highest probability class
+# according to the trained classifier for each input example.
+predicted_classes = model.predict_classes(X_test)
+
+# Check which items we got right / wrong
+correct_indices = np.nonzero(predicted_classes == y_test)[0]
+incorrect_indices = np.nonzero(predicted_classes != y_test)[0]
+
+plt.figure()
+for i, correct in enumerate(correct_indices[:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(X_test[correct].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[correct], y_test[correct]))
+    
+plt.figure()
+for i, incorrect in enumerate(incorrect_indices[:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(X_test[incorrect].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[incorrect], y_test[incorrect]))
+'''
 
